78795bd90e6baf26b986c5e2c643cec4b7f1190f
Aaron Gonzales <xysmas@gmail.com>
2015-11-13 12:56:12 -0700

    i think it works minus an assert

--- build/marshalling_test.c
+++ build/marshalling_test.c
@@ -5,31 +5,52 @@
 #include <assert.h>
 
 #include "srpc.h"
+#include "dbg.h"
 #include "marshalling.h"
 
 
 void test_pack(Srpc_Arg *in, unsigned char *buf){
 
-    printf("-----Testing argument packing-----\n\n");
+    debug("-----Testing argument packing-----\n\n");
     int err = Srpc_pack_args(in, buf);
 
-    printf("-- packing status-----: %s\n\n\n", Srpc_StatusMsg(err));
+    debug("-- packing status-----: %s\n\n\n", Srpc_StatusMsg(err));
 
 }
 
-void test_unpack_type(unsigned char *buf, Srpc_Arg *in){
+void test_unpack_type(Srpc_Type type){
 
+    Srpc_Arg *in_arg;
+    Srpc_Arg *out_arg;
+    unsigned char *out_buf;
+    unsigned char string[] = "fuck";
+    int myint = 42;
+    /* out_buf = (unsigned char *) malloc(sizeof(12)); */
     int dtype;
+    log_info("----test marshalling----: unpacking type\n");
+
+    if (type == SRPC_TYPE_INT){
+        debug("making int arg");
+        in_arg = arg_maker(SRPC_TYPE_INT, sizeof(myint), (void *) (intptr_t) myint);
+        print_args(in_arg);
+        out_buf = (unsigned char *) malloc((3 * sizeof(int)));
+    }
+    if (type == SRPC_TYPE_DATA){
+        debug("making data arg");
+        in_arg = arg_maker(SRPC_TYPE_DATA, sizeof(string), (void *) string);
+        print_args(in_arg);
+        out_buf = (unsigned char *) malloc(sizeof(string + sizeof(int) + sizeof(int)));
+    }
+
     /* print_args(in); */
-    printf("----test marshalling----: unpacking type\n");
-    dtype = srpc_unpack_type(buf);
-    printf("Dtype unpacked: %d\n", dtype);
+    int err = Srpc_pack_args(in_arg, out_buf);
+    debug("buffer after packing");
+    print_buffer_bytes(out_buf, in_arg->size + 2 * sizeof(int));
+    dtype = srpc_unpack_type(out_buf);
 
 
-    assert(in->type == dtype);
-/*     Srpc_Type type; // Type of argument */
-/*     unsigned int size; // Size of arg, in bytes. */
-/*     void *value; // Value */
+    assert(in_arg->type == dtype);
+    log_info ("Unpack TYPE %d TEST PASSED: ", type);
 
 }
 
@@ -37,9 +58,9 @@ void test_unpack_size(unsigned char *buf, Srpc_Arg *in){
 
     int size;
     /* print_args(in); */
-    printf("----test marshalling----: unpacking size\n");
+    debug("----test marshalling----: unpacking size\n");
     int dtype = srpc_unpack_type(buf);
-    printf("Dtype unpacked: %d\n", dtype);
+    debug("Dtype unpacked: %d\n", dtype);
 
 
     assert(in->type == dtype);
@@ -51,58 +72,57 @@ void test_unpack_size(unsigned char *buf, Srpc_Arg *in){
 
 
 
-void test_unpack(unsigned char *buf, Srpc_Arg *in){
-
+void test_unpack(Srpc_Type type){
+    Srpc_Arg *in;
     Srpc_Arg *dest;
-    print_args(in);
-    printf("unpacking buffer into outarg\n");
-    /* Srpc_unpack_args(buf, out); */
-    dest = unpack_args(buf);
+    unsigned char *out_buf;
+    unsigned char string[] = "fuck";
+    int myint = 42;
+    /* out_buf = (unsigned char *) malloc(sizeof(12)); */
+    int dtype;
+    log_info("----test marshalling----: unpacking type\n");
+
+    if (type == SRPC_TYPE_INT){
+        debug("making int arg");
+        in = arg_maker(SRPC_TYPE_INT, sizeof(myint), (void *) (intptr_t) myint);
+        print_args(in);
+        out_buf = (unsigned char *) malloc((3 * sizeof(int)));
+    }
+    if (type == SRPC_TYPE_DATA){
+        debug("making data arg");
+        in = arg_maker(SRPC_TYPE_DATA, sizeof(string), (void *) string);
+        print_args(in);
+        out_buf = (unsigned char *) malloc(sizeof(string + sizeof(int) + sizeof(int)));
+    }
+
+    /* print_args(in); */
+    int err = Srpc_pack_args(in, out_buf);
+
+    debug("buffer after packing");
+    print_buffer_bytes(out_buf, in->size + 2 * sizeof(int));
+
+    dest = unpack_args(out_buf);
+    debug("printing dest arg");
+    print_args(dest);
 
     assert(in->type == dest->type);
     assert(in->size == dest->size);
-    assert(in->value == dest->value);
-    printf("Unpacking works:\n");
-    print_args(dest);
-/*     Srpc_Type type; // Type of argument */
-/*     unsigned int size; // Size of arg, in bytes. */
-/*     void *value; // Value */
+    assert((char *) in->value == (char *) dest->value);
+
+    log_info ("Unpack %d TEST PASSED: ", type);
+
 
 }
 
 
 int main(void){
 
+    test_unpack_type(SRPC_TYPE_INT);
+    test_unpack_type(SRPC_TYPE_DATA);
 
-    printf("Initialzing arg\n");
-    Srpc_Arg *in_arg;
-    Srpc_Arg *out_arg;
-    unsigned char *out_buf;
-    out_buf = (unsigned char *) malloc(sizeof(12));
+    test_unpack(SRPC_TYPE_INT);
+    test_unpack(SRPC_TYPE_DATA);
 
-    unsigned char string[] = "fuck";
-    int myint = 100000000;
-
-
-
-    in_arg = arg_maker(SRPC_TYPE_INT, sizeof(myint), (void *) (intptr_t) myint);
-    print_args(in_arg);
-    /* printf("Size of 'fuck': %lu\n", sizeof(myint)); */
-
-    test_pack(in_arg, out_buf);
-    print_buffer_bytes(out_buf, 12);
-    out_arg = _arg_maker();
-
-    printf("Testing unpacking args\n");
-    test_unpack_type(out_buf, in_arg);
-    test_unpack(out_buf, in_arg);
-    printf("Making a new arg\n");
-
-    in_arg = arg_maker(SRPC_TYPE_DATA, sizeof(string), (void *) string);
-    print_args(in_arg);
-    out_buf = (unsigned char *) malloc(sizeof(string));
-    test_unpack_type(out_buf, in_arg);
-    test_unpack(out_buf, in_arg);
 
 
     return 0;
--- src/marshalling.c
+++ src/marshalling.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <arpa/inet.h>
+#include "dbg.h"
 
 
 #include "srpc.h"
@@ -30,20 +31,20 @@ unsigned char * arg_to_c(Srpc_Arg *arg){
 }
 
 void print_args(Srpc_Arg *arg){
-  printf("SRPC ARG:\n");
-  printf("---- type: %d \n", arg->type);
-  printf("---- size: %d \n", arg->size);
+  debug("SRPC ARG:\n");
+  debug("---- type: %d \n", arg->type);
+  debug("---- size: %d \n", arg->size);
   if (arg->value != NULL){
     if (arg->type == SRPC_TYPE_INT)
-      printf("---- value: %d \n", arg->value);
+      debug("---- value: %d \n", arg->value);
     if (arg->type == SRPC_TYPE_DATA)
-      printf("---- value: %s\n", arg->value);
+      debug("---- value: %s\n", arg->value);
   }
   else {
 
-      printf("---- value: is null\n\n");
+      debug("---- value: is null\n\n");
   }
-  printf("---- \n");
+  debug("---- \n");
 
 }
 
@@ -67,18 +68,6 @@ Srpc_Arg *arg_maker(Srpc_Type t, unsigned int size, void *data){
     return arg;
 }
 
-int srpc_unpack(unsigned char *buf)
-{
-  unsigned char bytes[4];
-  int dtype = 0;
-
-  memcpy(bytes, buf, sizeof(int));
-  dtype |= bytes[3];
-  print_buffer_bytes(bytes, sizeof(int));
-
-  return dtype;
-
-}
 
 int srpc_unpack_type(unsigned char *buf)
 {
@@ -87,10 +76,12 @@ int srpc_unpack_type(unsigned char *buf)
 
   /* bytes = (unsigned char *) malloc(sizeof(int)); */
 
+  /* print_buffer_bytes(buf, 4* sizeof(int)); */
   /* memmove(&dtype, buf, sizeof(int)); */
   memcpy(bytes, buf, sizeof(int));
-  dtype |= *(buf + 3);
-  print_buffer_bytes(bytes, sizeof(int));
+  dtype |= bytes[3];
+  /* print_buffer_bytes(buf, sizeof(int)); */
+  /* print_buffer_bytes(bytes, sizeof(int)); */
   /* free(bytes); */
 
   return dtype;
@@ -102,15 +93,8 @@ int srpc_unpack_argsize(unsigned char *buf)
   unsigned char bytes[4];
   int argsize = 0;
 
-  /* bytes = (unsigned char *) malloc(sizeof(int)); */
-
-  /* memmove(&dtype, buf, sizeof(int)); */
   memcpy(bytes, buf, sizeof(int));
-  /* buf += sizeof(int); */
-  /* dtype = bytes[0] | ( (int)bytes[1] << 8 ) | ( (int)bytes[2] << 16 ) | ( (int)bytes[3] << 24 ); */
   argsize = bytes[3];
-  /* print_buffer_bytes(bytes, sizeof(int)); */
-  /* free(bytes); */
 
   return argsize;
 
@@ -132,6 +116,8 @@ void * srpc_unpack_value(unsigned char *buf, Srpc_Type type, int size){
       case SRPC_TYPE_DATA:
           data = (char *) malloc(size);
           memcpy(data, buf, size);
+          debug("printing data buffer for size %d", size);
+          print_buffer_bytes(data, size);
           return (void *)data;
 
       case SRPC_TYPE_NONE:
@@ -169,7 +155,7 @@ Srpc_Status Srpc_pack_args(Srpc_Arg *pa, unsigned char *buf){
     switch (pa->type){
 
       case SRPC_TYPE_INT:
-        printf("debug: pack args: switching on int type\n");
+        /* debug("pack args: switching on int type\n"); */
         // assign based on size
         switch (pa->size){
           case 1:
@@ -188,6 +174,7 @@ Srpc_Status Srpc_pack_args(Srpc_Arg *pa, unsigned char *buf){
         break;
 
       case SRPC_TYPE_DATA:
+        /* debug("pack args: switching on data type\n"); */
         memcpy(p, pa->value, pa->size);
         break;
       default:
@@ -208,12 +195,11 @@ void print_buffer_bytes(unsigned char *bufptr, int n){
     int i = 0;
     unsigned char *byte_array = bufptr;
 
-    printf("---Printing the bytes in the buffer---\n");
     while (i < n)
     {
-         printf("%02X", (int)byte_array[i]);
+         printf("%02X ", (int)byte_array[i]);
          i++;
-         if (i % 4 == 0) printf(" ");
+         if (i % 4 == 0) printf(" -- ");
     }
     printf("\n\n\n");
 
